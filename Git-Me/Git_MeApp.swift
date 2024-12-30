3692752aa330218ebb9ffadaaea0432e3fc6b331
“thebariscc” <thebariscc@gmail.com>
2024-12-31 01:15:08 +0300

    Initial Commit

---
+++ Git-Me/Git_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_MeApp.swift
+//  Git-Me
+//
+//  Created by Barış Can Ceylan on 31.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Git_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
